9e0980dc8cd8d3763a21a1747fa2ee56efcb1c28
lawalfrontend developer <Olawal@dervac.com>
2024-03-19 22:10:45 +0100

    changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,12 +5,8 @@ module.exports = {
     extend: {
       backgroundImage: {
         'asset-image': "url('/src/images/asset 36.jpeg')",
-        'asset-image-1': "url('/src/images/asset 14.webp')"},
-        colors: {
-          blue: {
-            950: '#17275c',
-          },
-        }
+        'asset-image-1': "url('/src/images/asset 14.webp')",    
+      },
       }
     },
   plugins: [],
