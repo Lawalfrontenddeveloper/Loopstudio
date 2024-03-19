ddbfb3823344191d083f0874026d84ab296c0ba6
lawal frontend developer <Olawal@dervac.com>
2024-03-19 12:13:27 +0100

    changes

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'asset-image': "url('/src/images/asset 36.jpeg')",
+        'asset-image-1': "url('/src/images/asset 14.jpeg')",
+        colors: {
+          blue: {
+            950: '#17275c',
+          },
+        }
+      }
+    },
+  },
+  plugins: [],
+}
